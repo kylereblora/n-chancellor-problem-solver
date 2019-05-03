764375ce93467b4b61ebef7c85fbeb96c667f72d
User <moshe.nacu@gmail.com>
2019-05-03 14:01:55 +0800

    resolved issue: finding no solutions

--- baticos_nicolas_reblora.c
+++ baticos_nicolas_reblora.c
@@ -19,7 +19,7 @@ int main(){
 	//	Process: Find All Solutions (per board)
 	for(int i=0; i<numOfBoards; i++){
 
-		printf("\n -- Board #%d --\n", i+1);
+		printf("\n ********** Board #%d **********\n", i+1);
 		fscanf(fp, "%d\n", &n);
 		printf(" Matrix Size: %d\n", n);
 
@@ -36,78 +36,129 @@ int main(){
 			if (board[j] == 'C') chancellorStack[chancellorC++] = j;
 		}
 
-	/****************************************** SOLUTION ******************************************/
-
-		int start, move;
-		int nopts[n+2-chancellorC];	
-		int options[n+2-chancellorC][size+2-chancellorC];
-		int i, candidate, solutions = 0;
-
-		move = start = 0; 
-		nopts[start] = 1;
-		
-		while (nopts[start] > 0)
-		{
-			if(nopts[move]>0) 
-			{
-				move++;
-				nopts[move]=0;
-
-				if(move==n+1-chancellorC)	//	SOLUTION FOUND!
+		int hasNoSolution = 0;
+		for (int m=0; m<chancellorC; m++) {
+			for (int k=m; k<chancellorC; k++) {
+				if (m!=k)
 				{
-					solutions++;
-					for (int i=0; i<chancellorC; i++) printf("%2i\t",chancellorStack[i]);
-					for(i=1;i<move;i++)
-						printf("%2i\t",options[i][nopts[i]]);
-					printf("\n");
-				}
-				else if(move == 1){
-					for(candidate = size-1; candidate >=0; candidate --) 
-					{
-						int x = candidate / n;
-						int y = candidate % n;
-						int isValid = 1;
-
-						for (int i = 0; i < chancellorC; i++) {
-							int a = chancellorStack[i] / n;
-							int b = chancellorStack[i] % n;
-							if (a == x || b == y || (abs(x-a)==2 && abs(y-b)==1) || (abs(x-a)==1 && abs(y-b)==2)) isValid = 0;
-						}
-
-						if (isValid==1) {
-							nopts[move]++;
-							options[move][nopts[move]] = candidate;							
-						}
+					int x = chancellorStack[m] / n;
+					int y = chancellorStack[m] % n;
+					int a = chancellorStack[k] / n;
+					int b = chancellorStack[k] % n;
+					if (a == x || b == y || (abs(x-a)==2 && abs(y-b)==1) || (abs(x-a)==1 && abs(y-b)==2)) {
+						hasNoSolution = 1;
 					}
 				}
-				else
-				{
-					for(candidate=1;candidate<=nopts[move-1];candidate++)
-					{
-						int topOfStack = options[move-1][nopts[move-1]];
-						int x = topOfStack / n;
-						int y = topOfStack % n;
-
-						int current = options[move-1][candidate];
-						int a = current / n;
-						int b = current % n;
-						int isValid = 1;
-
-						//	Rook / Knight Move Checking
-						if (!(a == x || b == y || (abs(x-a)==2 && abs(y-b)==1) || (abs(x-a)==1 && abs(y-b)==2))) {
-							nopts[move]++;
-							options[move][nopts[move]] = current; 
-						}
-					}
-				}
-			}
-			else 
-			{
-				move--;
-				nopts[move]--;
 			}
 		}
 
+	/****************************************** SOLUTION ******************************************/
+
+		int solutions = 0;
+		if (hasNoSolution==0)
+		{
+			int start, move;
+			int nopts[n+2-chancellorC];	
+			int options[n+2-chancellorC][size+2-chancellorC];
+			int i, candidate;
+
+			move = start = 0; 
+			nopts[start] = 1;
+			
+			while (nopts[start] > 0)
+			{
+				if(nopts[move]>0) 
+				{
+					move++;
+					nopts[move]=0;
+
+					if(move==n+1-chancellorC)	//	SOLUTION FOUND!
+					{
+						printf(" Solution %d -- (\t", (solutions++)+1);
+						
+						int *solution = (int*) malloc(sizeof(int)*n);
+						int count = 0;
+						
+						for (int i=0; i<chancellorC; i++){
+							solution[count] = chancellorStack[i];
+							count++;
+							printf("%2i\t",chancellorStack[i]);
+						}
+						for(i=1;i<move;i++) {
+							solution[count] = options[i][nopts[i]];
+							count++;
+							printf("%2i\t",options[i][nopts[i]]);							
+						}
+						printf(")\n");
+
+						// -- Board Print
+						for (int i = 0; i < size; i++)
+						{
+							int hasNoC = 1;
+							for (int j = 0; j < count; j++)
+							{
+								if (i==solution[j]) {
+									hasNoC = 0;
+									break;
+								}
+							}
+
+							if (i%n==0 && i>=n) printf("\n");
+							if(hasNoC==1)
+								printf("0");
+							else
+								printf("C");
+							
+						} printf("\n\n");
+
+					}
+					else if(move == 1){
+						for(candidate = size-1; candidate >=0; candidate --) 
+						{
+							int x = candidate / n;
+							int y = candidate % n;
+							int isValid = 1;
+
+							for (int i = 0; i < chancellorC; i++) {
+								int a = chancellorStack[i] / n;
+								int b = chancellorStack[i] % n;
+								if (a == x || b == y || (abs(x-a)==2 && abs(y-b)==1) || (abs(x-a)==1 && abs(y-b)==2)) isValid = 0;
+							}
+
+							if (isValid==1) {
+								nopts[move]++;
+								options[move][nopts[move]] = candidate;							
+							}
+						}
+					}
+					else
+					{
+						for(candidate=1;candidate<=nopts[move-1];candidate++)
+						{
+							int topOfStack = options[move-1][nopts[move-1]];
+							int x = topOfStack / n;
+							int y = topOfStack % n;
+
+							int current = options[move-1][candidate];
+							int a = current / n;
+							int b = current % n;
+							int isValid = 1;
+
+							//	Rook / Knight Move Checking
+							if (!(a == x || b == y || (abs(x-a)==2 && abs(y-b)==1) || (abs(x-a)==1 && abs(y-b)==2))) {
+								nopts[move]++;
+								options[move][nopts[move]] = current; 
+							}
+						}
+					}
+				}
+				else 
+				{
+					move--;
+					nopts[move]--;
+				}
+			}
+		}
 		printf(" No. of Solutions Found: %d\n", solutions);
 	}
 
