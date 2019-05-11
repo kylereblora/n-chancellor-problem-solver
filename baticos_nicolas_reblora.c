881a7758b7d9686f7d68e81166c9e74e13e40c33
kylereblora <kbreblora@up.edu.ph>
2019-05-11 15:47:23 +0800

    Add javascript implementation in solver.js

--- baticos_nicolas_reblora.c
+++ baticos_nicolas_reblora.c
@@ -19,141 +19,178 @@ int main(){
 	//	Process: Find All Solutions (per board)
 	for(int i=0; i<numOfBoards; i++){
 
-		printf("\n -- Board #%d --\n", i+1);
+		printf("\n ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////\n");
+		printf(" /                                                            Board #%d                                                            /\n", i+1);
+		printf(" ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////\n");
 		fscanf(fp, "%d\n", &n);
-		printf(" Matrix Size: %d\n", n);
+		printf("\n MATRIX SIZE: %d\n", n);
 
+		//	Chancellors Creation
+		char *chancellorStack = (char*) malloc(sizeof(char)*n);	
+		int chancellorC = 0;
+		
 		//	Matrix Creation
 		int size = n*n;
 		char *board = (char*) malloc(sizeof(char)*size);	
+		printf(" INITIAL BOARD CONFIGURATION \n ");
 		for(int j=0; j<size; j++){
 			fscanf(fp, "%c", &board[j]);
-			if(j!=0 && (j+1)%n == 0) fscanf(fp, "\n");
-		}
+			printf("%c", board[j]);
+			if(j!=0 && (j+1)%n == 0) {
+				fscanf(fp, "\n");
+				printf("\n ");
+			}
+			if (board[j] == 'C') chancellorStack[chancellorC++] = j;
+		} printf("\n");
 
-
-	/****************************************** Solution ******************************************/
-
-		int start, move;
-		int nopts[n+2];				//	top of stack
-		int options[n+2][size+2];	//	matrix
-		int i, candidate, solutions = 0;
-
-		move = start = 0; 
-		nopts[start]= 1;
-		
-		while (nopts[start] >0) //while dummy stack is not empty
-		{
-			if(nopts[move]>0) 
-			{
-				move++;
-				nopts[move]=0; //initialize new move
-
-				if(move==n+1) //solution found!
+		//	Board Error!
+		int hasNoSolution = 0, numberOfErrors = 0;
+		for (int m=0; m<chancellorC; m++) {
+			for (int k=m; k<chancellorC; k++) {
+				if (m!=k)
 				{
-					solutions++;
-					// for(i=1;i<move;i++)
-					// 	printf("%2i\t",options[i][nopts[i]]);
-					// printf("\n");
-				}
-				else if(move == 1){
-					for(candidate = size-1; candidate >=0; candidate --) 
-					{
-						nopts[move]++;
-						options[move][nopts[move]] = candidate;
-					}
-				}
-				else{
-
-					for(candidate=1;candidate<=nopts[move-1];candidate++)
-					{
-						// get TOS, x position and y position
-						int topOfStack = options[move-1][nopts[move-1]];
-						int x = topOfStack / n;
-						int y = topOfStack % n;
-						// printf("Chancellor: %d\n", topOfStack);
-
-						// Knight Moves Exclusion
-						int knightMoves[8];
-						int p,q;
-
-							//	Up-Up-Left
-							p = x-2; q = y-1;
-							if (p < 0 || q < 0) knightMoves[0] = -1;
-							else knightMoves[0] = (p*n)+q;
-
-							//	Up-Up-Right
-							p = x-2; q = y+1;
-							if (p < 0 || q >= n) knightMoves[1] = -1;
-							else knightMoves[1] = (p*n)+q;
-
-							//	Up-Left-Left
-							p = x-1; q = y-2;
-							if (p < 0 || q < 0) knightMoves[2] = -1;
-							else knightMoves[2] = (p*n)+q;
-
-							//	Up-Right-Right
-							p = x-1; q = y+2;
-							if (p < 0 || q >= n) knightMoves[3] = -1;
-							else knightMoves[3] = (p*n)+q;
-
-							//	Down-Left-Left
-							p = x+1; q = y-2;
-							if (p >= n || q < 0) knightMoves[4] = -1;
-							else knightMoves[4] = (p*n)+q;
-
-							//	Down-Right-Right
-							p = x+1; q = y+2;
-							if (p >= n || q >= n) knightMoves[5] = -1;
-							else knightMoves[5] = (p*n)+q;
-
-							//	Down-Down-Left
-							p = x+2; q = y-1;
-							if (p >= n || q < 0) knightMoves[6] = -1;
-							else knightMoves[6] = (p*n)+q;
-
-							//	Down-Down-Right
-							p = x+2; q = y+1;
-							if (p >= n || q >= n) knightMoves[7] = -1;
-							else knightMoves[7] = (p*n)+q;
-
-						int current = options[move-1][candidate];
-						int a = current / n;
-						int b = current % n;
-						int isValid = 1;
-
-						// printf("\nasssessing...%d\n", current);
-						for(int j = 0; j < 8; j++) {
-							if(knightMoves[j] == current) {
-								// printf("Conflict knight!\n");
-								isValid = 0; break;
+					int x = chancellorStack[m] / n;
+					int y = chancellorStack[m] % n;
+					int a = chancellorStack[k] / n;
+					int b = chancellorStack[k] % n;
+					if (a == x || b == y || (abs(x-a)==2 && abs(y-b)==1) || (abs(x-a)==1 && abs(y-b)==2)) {
+						if (numberOfErrors==0) {
+							printf("------------------------------------------------------------------------------------------------\n");
+							printf(" There exist a chancellor(C) in the configuration that violates the rules for finding a solution!\n");							
+							printf(" ------------------------------------------------------------------------------------------------\n");
+							printf("\n **************************************************\n");
+							printf(" * Please resolve conflict between the following: *\n *                                                *\n");
+						}
+						printf(" *  --> CHANCELLOR_A[%d][%d] & CHANCELLOR_B[%d][%d]   *\n", x,y,a,b);
+						// printf(" *            xxxx   *\n");
+						printf(" *            ");
+						for (int p = 0; p < size; p++) {
+							if (p%n==0 && p>=n) printf("                                *\n *            ");
+							if (p==chancellorStack[m] || p==chancellorStack[k]) {
+								printf("X");
+							} else {
+								printf("%c", board[p]);
 							}
 						}
-
-						// printf("%d==%d || %d==%d\n", a,x,b,y);
-						if (a == x || b == y) {
-							// printf("Conflict row/col!\n");
-							isValid = 0;
-						}
-
-						if (isValid == 1) {
-							nopts[move]++;
-							options[move][nopts[move]] = current; 
-							// printf("valid: %d ", current);
-						}
+						printf("                                *\n *                                                *\n");
+						hasNoSolution = 1;
+						numberOfErrors++;
 					}
-				// printf("\n");
 				}
-			}
-			else 
-			{
-				// printf("backtracking...\n");
-				move--;
-				nopts[move]--;
 			}
 		}
 
+	/****************************************** SOLUTION ******************************************/
+
+		int solutions = 0;
+		if (hasNoSolution==0)
+		{
+			int start, move;
+			int nopts[n+2-chancellorC];	
+			int options[n+2-chancellorC][size+2-chancellorC];
+			int i, candidate;
+
+			move = start = 0; 
+			nopts[start] = 1;
+			
+			while (nopts[start] > 0)
+			{
+				if(nopts[move]>0) 
+				{
+					move++;
+					nopts[move]=0;
+
+					if(move==n+1-chancellorC)	//	SOLUTION FOUND!
+					{
+						printf(" Solution %d -- (\t", (solutions++)+1);
+						
+						int *solution = (int*) malloc(sizeof(int)*n);
+						int count = 0;
+						
+						for (int i=0; i<chancellorC; i++){
+							solution[count] = chancellorStack[i];
+							count++;
+							printf("%2i\t",chancellorStack[i]);
+						}
+						for(i=1;i<move;i++) {
+							solution[count] = options[i][nopts[i]];
+							count++;
+							printf("%2i\t",options[i][nopts[i]]);							
+						}
+						printf(")\n");
+
+						// -- Board Print
+						for (int i = 0; i < size; i++)
+						{
+							int hasNoC = 1;
+							for (int j = 0; j < count; j++)
+							{
+								if (i==solution[j]) {
+									hasNoC = 0;
+									break;
+								}
+							}
+
+							if (i%n==0 && i>=n) printf("\n");
+							if(hasNoC==1)
+								printf("0");
+							else
+								printf("C");
+							
+						} printf("\n\n");
+
+					}
+					else if(move == 1){
+						for(candidate = size-1; candidate >=0; candidate --) 
+						{
+							int x = candidate / n;
+							int y = candidate % n;
+							int isValid = 1;
+
+							for (int i = 0; i < chancellorC; i++) {
+								int a = chancellorStack[i] / n;
+								int b = chancellorStack[i] % n;
+								if (a == x || b == y || (abs(x-a)==2 && abs(y-b)==1) || (abs(x-a)==1 && abs(y-b)==2)) isValid = 0;
+							}
+
+							if (isValid==1) {
+								nopts[move]++;
+								options[move][nopts[move]] = candidate;							
+							}
+						}
+					}
+					else
+					{
+						for(candidate=1;candidate<=nopts[move-1];candidate++)
+						{
+							int topOfStack = options[move-1][nopts[move-1]];
+							int x = topOfStack / n;
+							int y = topOfStack % n;
+
+							int current = options[move-1][candidate];
+							int a = current / n;
+							int b = current % n;
+							int isValid = 1;
+
+							//	Rook / Knight Move Checking
+							if (!(a == x || b == y || (abs(x-a)==2 && abs(y-b)==1) || (abs(x-a)==1 && abs(y-b)==2))) {
+								nopts[move]++;
+								options[move][nopts[move]] = current; 
+							}
+						}
+					}
+				}
+				else 
+				{
+					move--;
+					nopts[move]--;
+				}
+			}
+		}
+		printf(" **************************************************\n\n");
+		printf(" =========================\n");
 		printf(" No. of Solutions Found: %d\n", solutions);
+		printf(" Errors Found: %d\n", numberOfErrors);
 	}
 
 	printf("\n == PROGRAM END ==\n\n");
--- client/src/solver.js
+++ client/src/solver.js
@@ -1,5 +1,158 @@
 export const solver = (file) => {
-    console.log(file);
+    // console.log(file);
 
+    let filecontent = file.split('\n');
+
+    // create a copy of the content of the files
+    let lines = filecontent.slice();
+    
+    let numOfBoards = parseInt(lines[0]);
+    
+    // remove the first element in lines array
+    lines.shift();
+
+    for (let i=0; i < numOfBoards; i++) {
+        
+        let n = parseInt(lines[0]);
+        lines.shift();
+
+        console.log(n);
+        console.log(lines);
+        
+
+        // Chancellors Creation
+        let chancellorStack = [n];
+        let chancellorC = 0;
+
+
+        let stringifiedMatrix = ''
+        for (let j=0; j<n; j++) {
+
+            // remove the \n from each line
+            stringifiedMatrix += (lines[j].substring(0, n));
+        }
+
+        for (let j=0; j<n; j++) {
+            lines.shift();
+        }        
+
+
+        // Matrix Creation
+        let size = n*n
+        let board = []
+
+        
+        for (let j=0; j<stringifiedMatrix.length; j++) {
+            let currentChar = stringifiedMatrix[j]
+            
+            board.push(currentChar);
+            
+            if (currentChar === 'C') chancellorStack[chancellorC++] = j
+        }
+
+        console.log(board);
+
+        let hasNoSolution = 0, numberOfErrors = 0;
+
+        for (let m=0; m<chancellorC; m++) {
+            for (let k=m; k<chancellorC; k++) {
+                if (m!==k) {
+                    let x = parseInt(chancellorStack[m] / n);
+                    let y = parseInt(chancellorStack[m] % n);
+                    let a = parseInt(chancellorStack[k] % n);
+                    let b = parseInt(chancellorStack[k] % n);
+                    
+                    if (a === x || b === y || (Math.abs(x-a) === 2 && Math.abs(y-b) === 1) || (Math.abs(x-a) === 1 && Math.abs(y-b) === 2) ) {
+                        hasNoSolution = 1;
+                        numberOfErrors++;
+                    }
+                }
+            }
+        }
+
+        
+        let solutions = 0;
+
+        if (hasNoSolution === 0) {
+            let start, move;
+            let nopts = [];
+            let options = [...Array(n+2-chancellorC)].map(e => Array(size+2-chancellorC));
+            let i, candidate;
+
+            move = start = 0;
+            nopts[start] = 1;
+
+            console.log(options);
+            
+
+            while (nopts[start] > 0) {
+                if (nopts[move] > 0) {
+                    move++;
+                    nopts[move] = 0;
+
+                    // SOLUTION FOUND!
+                    if (move=== n+1-chancellorC) {
+                        console.log("Solution", (solutions++)+1);
+
+                        let count = 0;
+                        for (let i=0; i<chancellorC; i++){
+							// solution[count] = chancellorStack[i];
+							count++;
+							console.log(chancellorStack[i]);
+						}
+
+                        
+                    } 
+                    else if (move === 1) {
+                        for (candidate=size-1; candidate >= 0; candidate--) {
+                            let x = parseInt(candidate / n);
+                            let y = parseInt(candidate % n);
+                            let isValid = 1;
+
+                            for (let i = 0; i < chancellorC; i++) {
+                                let a = parseInt(chancellorStack[i] / n);
+                                let b = parseInt(chancellorStack[i] % n);
+
+                                if (a === x || b === y || (Math.abs(x-a) === 2 
+                                && Math.abs(y-b) === 1) || (Math.abs(x-a) === 1 
+                                && Math.abs(y-b) === 2)) isValid = 0;
+                                
+                            }
+
+                            if (isValid === 1) {
+                                nopts[move]++;
+                                options[move][nopts[move]] = candidate;
+                            }
+                        }
+                    }
+                    else {
+                        for (candidate=1; candidate <= nopts[move-1]; candidate++) {
+                            let topOfStack = options[move-1][nopts[move-1]];
+                            let x = parseInt(topOfStack / n);
+                            let y = parseInt(topOfStack % n);
+
+                            let current = options[move-1][candidate];
+                            let a = parseInt(current / n);
+                            let b = parseInt(current % n);
+                            let isValid = 1;
+
+                            // Rook / Knight Move Checking
+
+                            if (!(a===x || b===y || (Math.abs(x-a === 2) && Math.abs(y-b) === 1) || (Math.abs(x-a) === 1 && Math.abs(y-b)=== 2))) {
+                                nopts[move]++;
+                                options[move][nopts[move]] = current;
+                            }
+                        }
+                    }
+                }
+                else {
+                    move--;
+                    nopts[move]--;
+                }
+            }            
+        }
+        
+        console.log("No. of solutions found: " + solutions);
+    }
     
 }
