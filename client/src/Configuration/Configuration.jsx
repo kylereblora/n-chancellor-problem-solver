f3d93d80a1f5fd039a34fe47941c4c7f50fb917e
Kyle Reblora <kbreblora@up.edu.ph>
2019-05-21 13:18:20 +0800

    Add prev/next buttons

--- client/src/Configuration/Configuration.jsx
+++ client/src/Configuration/Configuration.jsx
@@ -3,30 +3,53 @@ import Solution from '../Solution/Solution';
 import './configuration.css'
 // import { Grid, Segment } from 'semantic-ui-react'
 
-const Configuration = (props) => {
-    const {solutionSet} = props;
-    console.log(solutionSet);
+
+
+class Configuration extends React.Component {
+    state = {
+        solutionSet: this.props.solutionSet,
+        activeIndex: 0,
+    }
+
+    handlePrevSolution = () => {
+        const {solutionSet, activeIndex} = this.state;
+        const newPointer = activeIndex === 0 ? solutionSet.length -1 : activeIndex - 1;
+        this.setState({activeIndex: newPointer});
+    }
+
+    handleNextSolution = () => {
+        const {solutionSet, activeIndex} = this.state;
+        const newPointer = activeIndex === solutionSet.length-1 ? 0 : activeIndex + 1;
+        this.setState({activeIndex: newPointer});
+    }
+
+    render() {
+        const {solutionSet, activeIndex} = this.state;
+        console.log(solutionSet);
+        
     
-
-    return (
-        <div>
-            {
-                solutionSet && solutionSet.length > 0 ?
-
-                solutionSet.map((solution,index) => {
-                    return (
-                        <div key={index} className="configBoard">
-                            <Solution specificSolution={solution} />
+        return (
+            <div>
+                {
+                    solutionSet && solutionSet.length > 0 ?
+    
+                    <div>
+                        <div className="configBoard">
+                            <Solution specificSolution={solutionSet[activeIndex]} />
                         </div>
-                    )
-                })
 
-                :
-
-                <p>No solution available.</p>
-            }
-        </div>
-    )
+                        <button className="prev" onClick={this.handlePrevSolution}>&#10094;</button>
+                        <button className="next" onClick={this.handleNextSolution}>&#10095;</button>
+                        
+                    </div>
+    
+                    :
+    
+                    <p>No solution available.</p>
+                }
+            </div>
+        )
+    }
 }
 
 export default Configuration
